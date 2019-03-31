25efe136cde89fef4e6b3a7fc1b1a996cf561b2c
Robert Schwartzberg <rds407@nyu.edu>
2019-03-31 19:32:49 -0400

    added basic enemy ai for player detection

---
+++ Game Design Final Project/Assets/Scripts/Layers.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Layers {
+	
+	public const int PLAYER = 8;
+	public const int ENEMY = 9;
+
+}
--- Game Design Final Project/Assets/Scripts/Player.cs
+++ Game Design Final Project/Assets/Scripts/Player.cs
@@ -8,13 +8,20 @@ public class Player : MonoBehaviour {
 
 	new Rigidbody rigidbody;
 
-	float TOP_SPEED = 5f;
-	const float ACCELERATION = 15f;
-	const float DECELERATION = 15f;
+	const float TOP_SPEED = 5f;
+	const float ACCELERATION = 10f;
+	const float DECELERATION = 20f;
+	const float ROTATION_SPEED = 360f;
+
+	const float TOP_SPEED_SQR = TOP_SPEED * TOP_SPEED;
+
 
 	void Awake() {
 		instance = this;
 		rigidbody = GetComponent<Rigidbody>();
+		if (gameObject.layer != Layers.PLAYER) {
+			Debug.LogWarning("The player is not set to the Player layer");
+		}
 	}
 
 	void Start() {
@@ -40,16 +47,21 @@ public class Player : MonoBehaviour {
 		if (Input.GetKey(KeyCode.A)) {
 			x -= 1;
 		}
-		Vector2 dir = new Vector2(x, y);
+		Vector3 dir = new Vector3(x, 0, y);
 		float accel;
 		if (x != 0 || y != 0) {
 			dir.Normalize();
-			accel = ACCELERATION;
+			float dot = Vector3.Dot(rigidbody.velocity.normalized, dir);
+			accel = Mathf.Lerp(DECELERATION, ACCELERATION, dot);
+			transform.localRotation = Quaternion.RotateTowards(transform.localRotation, Quaternion.LookRotation(dir), ROTATION_SPEED * Time.deltaTime);
 		}
 		else {
 			accel = DECELERATION;
 		}
 		rigidbody.velocity = Vector3.MoveTowards(rigidbody.velocity, dir * TOP_SPEED, accel * Time.deltaTime);
+		if (rigidbody.velocity.sqrMagnitude > TOP_SPEED_SQR) {
+			rigidbody.velocity = rigidbody.velocity.normalized * TOP_SPEED;
+		}
 	}
 
 }
