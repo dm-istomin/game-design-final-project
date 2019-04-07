cdf56f8bbb20e9734ecdb7bd242861c226a7f5c1
Dmitriy Istomin <distomin+git@pm.me>
2019-04-07 03:31:45 -0400

    add v1 of procedural generation code

--- Game Design Final Project/Assets/Scripts/Room.cs
+++ Game Design Final Project/Assets/Scripts/Room.cs
@@ -1,15 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Room : MonoBehaviour {
-	public float height, width;
-	public List<Vector2> exits;
-
-	void Start() {
-		/*
-		GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-		cube.transform.position = new Vector3(exits[0].x, exits[0].y, 0);
-		*/
-	}
+	public List<RoomConnector> exits;
+	public Tilemap floor;
 }
